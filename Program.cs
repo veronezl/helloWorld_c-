deae65819549360b2f1d4d670ab0633daa3032a2
lucasveronez <lucasveronez.dev@gmail.com>
2024-11-04 21:12:08 -0300

    add métodos para strings

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
+
 Console.WriteLine("Hello, World!");
 
 string aFriend = "Bill";
@@ -19,5 +21,18 @@ string secondFriend = "     Sage     ";
 Console.WriteLine($"My friends are {firstFriend} and {secondFriend.Trim()}");
 Console.WriteLine($"The name {firstFriend} has {firstFriend.Length} letters.");
 Console.WriteLine($"The name {secondFriend} has {secondFriend.Length} letters.");
+
+System.Console.WriteLine(firstFriend.StartsWith("Sa")); // Começa com "Sa"
+System.Console.WriteLine(firstFriend.EndsWith("ia"));   // Termina com "ia"
+System.Console.WriteLine(firstFriend.Contains("a"));    // Contém a letra "a"
+System.Console.WriteLine(firstFriend.IndexOf("a"));    // Posição da letra "a"
+System.Console.WriteLine(firstFriend.LastIndexOf("a")); // Última posição da letra "a"
+System.Console.WriteLine(firstFriend.Replace("a", "e")); // Troca a letra "a" pela letra "e"
+System.Console.WriteLine(firstFriend.ToUpper()); // Deixa tudo em maiúsculas
+System.Console.WriteLine(firstFriend.ToLower()); // Deixa tudo em minúsculas
+System.Console.WriteLine(firstFriend.Substring(0, 4)); // Pegar 4 caracteres a partir
+System.Console.WriteLine(firstFriend.Substring(4)); // Pegar todos os caracteres a partir
+System.Console.WriteLine(firstFriend.Split('a')); // Divide a string em substrings
+System.Console.WriteLine(firstFriend.Split('a')[0]); // Pegar a primeira substring
 
 
