ebaff9ea5db106906b2286c29a58263ce53d6589
veronezl <lucasveronez.dev@gmail.com>
2024-11-06 08:25:58 -0300

    add: revisão do if

--- Program.cs
+++ Program.cs
@@ -42,6 +42,7 @@ System.Console.WriteLine(firstFriend.Split('a')[0]); // Pegar a primeira substri
 
 // TRABALHANDO COM NÚMEROS EM C#
 
+/*
 int a = 2100000000;
 int b = 2100000000;
 long c = (long)a + (long)b;
@@ -55,8 +56,38 @@ float f = 38.2F;
 double g = e + f;
 
 System.Console.WriteLine($"The answer is {g:F3}");
+*/
+
+// TRABALHNDO COM IF
 
 
+int a = 5;
+int b = 3;
+int c = 4;
+if ((a + b + c > 10) && (a == b))
+{
+    Console.WriteLine("The answer is greater than 10");
+    Console.WriteLine("And the first number is equal to the second");
+}
+else
+{
+    Console.WriteLine("The answer is not greater than 10");
+    Console.WriteLine("Or the first number is not equal to the second");
+}
+
+int x = 5;
+int y = 3;
+int w = 4;
+if ((x + y + w > 10) || (x == y))
+{
+    Console.WriteLine("The answer is greater than 10");
+    Console.WriteLine("Or the first number is equal to the second");
+}
+else
+{
+    Console.WriteLine("The answer is not greater than 10");
+    Console.WriteLine("And the first number is not equal to the second");
+}
 
 
 
